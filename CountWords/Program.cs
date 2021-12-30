803d4b1d952196cb4a5f2f14c805b30fad6262ac
Евгений Гилев <euggil@yandex.ru>
2021-12-30 11:19:33 +0500

    Добавьте файлы проекта.

---
+++ CountWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CountWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText(@"C:\temp1\Text1.txt");
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] {' ', '\r', '\n'};
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            // выводим количество
+            Console.WriteLine(words.Length);
+            
+        }
+    }
+}
